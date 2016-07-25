5df18f024148f794fd3240ea5fc701ee50156130
Vladimir Vilisov <vvvalt@mail.ru>
2016-07-25 22:19:10 +0700

    Update MQTT_Audio_Amplifier.ino

--- MQTT_Audio_Amplifier.ino
+++ MQTT_Audio_Amplifier.ino
@@ -7,17 +7,20 @@ char b[2];
 byte mac[]    = { 0x0A, 0x2F, 0x30, 0x42, 0x39, 0x1F };
 byte server[] = { 192, 168, 1, 190 }; //IP Брокера
 byte ip[]     = { 192, 168, 1, 153 }; //IP Клиента (Arduino)
+
 ////////////////////////////////////////////////////////////////////////////
 void callback(char* topic, byte* payload, unsigned int length) {
-    payload[length] = '\0';
-    String strTopic = String(topic);
-    String strPayload = String((char*)payload);
-    callback_iobroker(strTopic, strPayload);
+  payload[length] = '\0';
+  String strTopic = String(topic);
+  String strPayload = String((char*)payload);
+  callback_iobroker(strTopic, strPayload);
 }
 
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
+
 #define id_connect "myhome-Audio_Amplifier"
+long previousMillis = 0;
 
 #define PIN_AMP_1 17
 #define PIN_AMP_2 16
@@ -42,42 +45,46 @@ byte vol_5 = EEPROM.read(5);
 byte shtd = EEPROM.read(6);
 byte fade = EEPROM.read(7);
 ///////////////Объявляем порты ввода-вывода
-const int start_DI_pin []= {0}; // Порты Ввода
-int n_DI_pin = sizeof(start_DI_pin) / sizeof(start_DI_pin[0])-1; //Вычисляем длинну массива
+const int start_DI_pin [] = {0}; // Порты Ввода
+int n_DI_pin = sizeof(start_DI_pin) / sizeof(start_DI_pin[0]) - 1; //Вычисляем длинну массива
 
-const int start_DO_pin []= {PIN_AMP_1, PIN_AMP_2, PIN_AMP_3, PIN_AMP_4, PIN_AMP_5, PIN_VOL_1, PIN_VOL_2, PIN_VOL_3, PIN_VOL_4, PIN_VOL_5, PIN_SHTD, PIN_FADE}; //Порты Вывода
-int n_DO_pin = sizeof(start_DO_pin) / sizeof(start_DO_pin[0])-1; //Вычисляем длинну массива
+const int start_DO_pin [] = {PIN_AMP_1, PIN_AMP_2, PIN_AMP_3, PIN_AMP_4, PIN_AMP_5, PIN_VOL_1, PIN_VOL_2, PIN_VOL_3, PIN_VOL_4, PIN_VOL_5, PIN_SHTD, PIN_FADE}; //Порты Вывода
+int n_DO_pin = sizeof(start_DO_pin) / sizeof(start_DO_pin[0]) - 1; //Вычисляем длинну массива
 
-void PubTopic (){
-    client.publish("myhome/Audio_Amplifier/save", "false");
-    client.publish("myhome/Audio_Amplifier/zone_1", "false");
-    client.publish("myhome/Audio_Amplifier/zone_2", "false");
-    client.publish("myhome/Audio_Amplifier/zone_3", "false");
-    client.publish("myhome/Audio_Amplifier/zone_4", "false");
-    client.publish("myhome/Audio_Amplifier/zone_5", "false");
-    client.publish("myhome/Audio_Amplifier/vol_1", "10");
-    client.publish("myhome/Audio_Amplifier/vol_2", "10");
-    client.publish("myhome/Audio_Amplifier/vol_3", "10");
-    client.publish("myhome/Audio_Amplifier/vol_4", "10");
-    client.publish("myhome/Audio_Amplifier/vol_5", "10");
-    client.publish("myhome/Audio_Amplifier/fade", "false");
-    client.publish("myhome/Audio_Amplifier/shtd", "false");
-    client.publish("myhome/Audio_Amplifier/connection", "true");
-  }
+void PubTopic () {
+  client.publish("myhome/Audio_Amplifier/save", "false");
+  client.publish("myhome/Audio_Amplifier/zone_1", "false");
+  client.publish("myhome/Audio_Amplifier/zone_2", "false");
+  client.publish("myhome/Audio_Amplifier/zone_3", "false");
+  client.publish("myhome/Audio_Amplifier/zone_4", "false");
+  client.publish("myhome/Audio_Amplifier/zone_5", "false");
+  client.publish("myhome/Audio_Amplifier/vol_1", "10");
+  client.publish("myhome/Audio_Amplifier/vol_2", "10");
+  client.publish("myhome/Audio_Amplifier/vol_3", "10");
+  client.publish("myhome/Audio_Amplifier/vol_4", "10");
+  client.publish("myhome/Audio_Amplifier/vol_5", "10");
+  client.publish("myhome/Audio_Amplifier/fade", "false");
+  client.publish("myhome/Audio_Amplifier/shtd", "false");
+  client.publish("myhome/Audio_Amplifier/connection", "true");
+}
 ////////////////////////////////////////////////////////////////////////////
 void setup() {
-  if (vol_1 == 255 && vol_2 == 255 && vol_3 == 255 && vol_4 == 255 && vol_5 == 255 && shtd == 255 && fade == 255){
-      vol_1 = 10;
-      vol_2 = 10;
-      vol_3 = 10;
-      vol_4 = 10;
-      vol_5 = 10;
-      shtd = 1;
-      fade = 0;
+  if (vol_1 == 255 && vol_2 == 255 && vol_3 == 255 && vol_4 == 255 && vol_5 == 255 && shtd == 255 && fade == 255) {
+    vol_1 = 10;
+    vol_2 = 10;
+    vol_3 = 10;
+    vol_4 = 10;
+    vol_5 = 10;
+    shtd = 1;
+    fade = 0;
   }
-    //Объявляем порты Digital inputs/outputs
-  for(int i=0 ;i<=n_DI_pin; i++) { pinMode (start_DI_pin [i], INPUT); }
-  for(int i=0 ;i<=n_DO_pin; i++) { pinMode (start_DO_pin [i], OUTPUT); }
+  //Объявляем порты Digital inputs/outputs
+  for (int i = 0 ; i <= n_DI_pin; i++) {
+    pinMode (start_DI_pin [i], INPUT);
+  }
+  for (int i = 0 ; i <= n_DO_pin; i++) {
+    pinMode (start_DO_pin [i], OUTPUT);
+  }
 
   Ethernet.begin(mac, ip);
   if (client.connect(id_connect)) {
@@ -86,13 +93,7 @@ void setup() {
   }
 }
 /////////////////////////////////////////////////////////////////////////
-void save(){
-    EEPROM.write(1, vol_1);
-    EEPROM.write(2, vol_2);
-    EEPROM.write(3, vol_3);
-    EEPROM.write(4, vol_4);
-    EEPROM.write(5, vol_5);
-}
+
 void loop() {
   client.loop();
 
