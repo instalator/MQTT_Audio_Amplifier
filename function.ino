46541dc23bd7a806aab2f161a59d542eeff3b7ec
Vladimir Vilisov <vvvalt@mail.ru>
2016-07-25 12:41:55 +0700

    Update function.ino

--- function.ino
+++ function.ino
@@ -21,5 +21,51 @@ void callback_iobroker(String strTopic, String strPayload) {
         }
   }
   //////////////////////////
-  
-}
+  else if (strTopic == "myhome/Audio_Amplifier/zone_2") {
+        if (strPayload == "true") {
+            digitalWrite(PIN_AMP_2, true);
+            client.publish("myhome/Audio_Amplifier/zone_2", "true");
+        } else {
+            digitalWrite(PIN_AMP_2, false);
+            client.publish("myhome/Audio_Amplifier/zone_2", "false");
+        }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_3") {
+        if (strPayload == "true") {
+            digitalWrite(PIN_AMP_3, true);
+            client.publish("myhome/Audio_Amplifier/zone_3", "true");
+        } else {
+            digitalWrite(PIN_AMP_3, false);
+            client.publish("myhome/Audio_Amplifier/zone_3", "false");
+        }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_4") {
+        if (strPayload == "true") {
+            digitalWrite(PIN_AMP_4, true);
+            client.publish("myhome/Audio_Amplifier/zone_4", "true");
+        } else {
+            digitalWrite(PIN_AMP_4, false);
+            client.publish("myhome/Audio_Amplifier/zone_4", "false");
+        }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_5") {
+        if (strPayload == "true") {
+            digitalWrite(PIN_AMP_5, true);
+            client.publish("myhome/Audio_Amplifier/zone_5", "true");
+        } else {
+            digitalWrite(PIN_AMP_5, false);
+            client.publish("myhome/Audio_Amplifier/zone_5", "false");
+        }
+  }
+  /////////////////////////////////////////////////////////
+  if (strTopic == "myhome/Audio_Amplifier/vol_1") {
+    //if (strPayload == "true") {
+        analogWrite(PIN_VOL_1, strPayload);
+        client.publish("myhome/Audio_Amplifier/vol_1", "true");
+    //}
+  }
+      
+  }
