12e4bc7f4e9b55d58cc32a09da0334ad285123f5
Vladimir Vilisov <vvvalt@mail.ru>
2016-09-04 18:12:31 +0700

    fix fade

--- MQTT_Audio_Amplifier.ino
+++ MQTT_Audio_Amplifier.ino
@@ -8,7 +8,6 @@ byte mac[]    = { 0x0A, 0x2F, 0x30, 0x42, 0x39, 0x1F };
 byte server[] = { 192, 168, 1, 190 }; //IP Брокера
 byte ip[]     = { 192, 168, 1, 153 }; //IP Клиента (Arduino)
 
-long previousMillis = 0;
 byte vol_1 = EEPROM.read(1);
 byte vol_2 = EEPROM.read(2);
 byte vol_3 = EEPROM.read(3);
@@ -23,22 +22,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
   String strPayload = String((char*)payload);
   callback_iobroker(strTopic, strPayload);
 }
+
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
 
 #define id_connect "myhome-Audio_Amplifier"
+long previousMillis = 0;
+
 #define PIN_AMP_1 17
 #define PIN_AMP_2 16
 #define PIN_AMP_3 15
 #define PIN_AMP_4 14
 #define PIN_AMP_5 20
+
 #define PIN_VOL_1 5
 #define PIN_VOL_2 3
 #define PIN_VOL_3 9
 #define PIN_VOL_4 6
 #define PIN_VOL_5 10
-#define PIN_SHTD 2
-#define PIN_FADE 4
+
+#define PIN_SHTD 4
+#define PIN_FADE 2
 ///////////////Объявляем порты ввода-вывода
 const int start_DI_pin [] = {0}; // Порты Ввода
 int n_DI_pin = sizeof(start_DI_pin) / sizeof(start_DI_pin[0]) - 1; //Вычисляем длинну массива
@@ -58,16 +62,8 @@ void PubTopic () {
   client.publish("myhome/Audio_Amplifier/vol_3", IntToChar(vol_3));
   client.publish("myhome/Audio_Amplifier/vol_4", IntToChar(vol_4));
   client.publish("myhome/Audio_Amplifier/vol_5", IntToChar(vol_5));
-  if (fade == 1){
-    client.publish("myhome/Audio_Amplifier/fade", "true");
-  } else {
-    client.publish("myhome/Audio_Amplifier/fade", "false");
-  }
-  if (shtd == 1){
-    client.publish("myhome/Audio_Amplifier/shtd", "true");
-  } else {
-    client.publish("myhome/Audio_Amplifier/shtd", "false");
-  }
+  client.publish("myhome/Audio_Amplifier/fade", IntToChar(fade));
+  client.publish("myhome/Audio_Amplifier/shtd", IntToChar(shtd));
   client.publish("myhome/Audio_Amplifier/connection", "true");
 }
 ////////////////////////////////////////////////////////////////////////////
@@ -78,8 +74,8 @@ void setup() {
     vol_3 = 10;
     vol_4 = 10;
     vol_5 = 10;
-    shtd = 1;
-    fade = 0;
+    shtd = 0;
+    fade = 1;
   } else {
     if (shtd > 0){shtd = 1;}
     if (fade > 0){fade = 1;}
