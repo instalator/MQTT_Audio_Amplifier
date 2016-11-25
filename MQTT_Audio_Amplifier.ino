43ff80d7e49c83746fa3b42c43cefbe060ad313e
Vladimir Vilisov <vvvalt@mail.ru>
2016-11-25 22:15:45 +0700

    Update MQTT_Audio_Amplifier.ino

--- MQTT_Audio_Amplifier.ino
+++ MQTT_Audio_Amplifier.ino
@@ -3,18 +3,16 @@
 #include <Ethernet.h>      // Ethernet shield
 #include <PubSubClient.h>  // MQTT 
 
+//#define SPI_HAS_EXTENDED_CS_PIN_HANDLING 8
+//#define ETHERNET_SHIELD_SPI_CS 8
+
 char b[2];
 byte mac[]    = { 0x0A, 0x2F, 0x30, 0x42, 0x39, 0x1F };
 byte server[] = { 192, 168, 1, 190 }; //IP Брокера
 byte ip[]     = { 192, 168, 1, 153 }; //IP Клиента (Arduino)
 
-byte vol_1 = EEPROM.read(1);
-byte vol_2 = EEPROM.read(2);
-byte vol_3 = EEPROM.read(3);
-byte vol_4 = EEPROM.read(4);
-byte vol_5 = EEPROM.read(5);
-byte shtd = EEPROM.read(6);
-byte fade = EEPROM.read(7);
+byte vol_1,vol_2,vol_3,vol_4,vol_5,shtd,fade;
+
 ////////////////////////////////////////////////////////////////////////////
 void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
@@ -33,7 +31,7 @@ long previousMillis = 0;
 #define PIN_AMP_2 16
 #define PIN_AMP_3 15
 #define PIN_AMP_4 14
-#define PIN_AMP_5 20
+#define PIN_AMP_5 7
 
 #define PIN_VOL_1 5
 #define PIN_VOL_2 3
@@ -43,12 +41,6 @@ long previousMillis = 0;
 
 #define PIN_SHTD 4
 #define PIN_FADE 2
-///////////////Объявляем порты ввода-вывода
-const int start_DI_pin [] = {0}; // Порты Ввода
-int n_DI_pin = sizeof(start_DI_pin) / sizeof(start_DI_pin[0]) - 1; //Вычисляем длинну массива
-
-const int start_DO_pin [] = {PIN_AMP_1, PIN_AMP_2, PIN_AMP_3, PIN_AMP_4, PIN_AMP_5, PIN_VOL_1, PIN_VOL_2, PIN_VOL_3, PIN_VOL_4, PIN_VOL_5, PIN_SHTD, PIN_FADE}; //Порты Вывода
-int n_DO_pin = sizeof(start_DO_pin) / sizeof(start_DO_pin[0]) - 1; //Вычисляем длинну массива
 
 void PubTopic () {
   client.publish("myhome/Audio_Amplifier/save", "false");
@@ -68,30 +60,34 @@ void PubTopic () {
 }
 ////////////////////////////////////////////////////////////////////////////
 void setup() {
+   vol_1 = EEPROM.read(1);
+   vol_2 = EEPROM.read(2);
+   vol_3 = EEPROM.read(3);
+   vol_4 = EEPROM.read(4);
+   vol_5 = EEPROM.read(5);
+   shtd = EEPROM.read(6);
+   fade = EEPROM.read(7);
   if (vol_1 == 255 && vol_2 == 255 && vol_3 == 255 && vol_4 == 255 && vol_5 == 255 && shtd == 255 && fade == 255) {
-    vol_1 = 10;
-    vol_2 = 10;
-    vol_3 = 10;
-    vol_4 = 10;
-    vol_5 = 10;
+    vol_1 = 30;
+    vol_2 = 30;
+    vol_3 = 30;
+    vol_4 = 30;
+    vol_5 = 30;
     shtd = 0;
     fade = 1;
   } else {
     if (shtd > 0){shtd = 1;}
     if (fade > 0){fade = 1;}
   }
-  //Объявляем порты Digital inputs/outputs
-  for (int i = 0 ; i <= n_DI_pin; i++) {
-    pinMode (start_DI_pin [i], INPUT);
-  }
-  for (int i = 0 ; i <= n_DO_pin; i++) {
-    pinMode (start_DO_pin [i], OUTPUT);
-  }
-  digitalWrite(PIN_AMP_1, 1);
-  digitalWrite(PIN_AMP_2, 1);
-  digitalWrite(PIN_AMP_3, 1);
-  digitalWrite(PIN_AMP_4, 1);
-  digitalWrite(PIN_AMP_5, 1);
+DDRD |= B11111100;
+DDRB |= B00000110;
+DDRC |= B00001111;
+  //PORTB |= B1100;
+  digitalWrite(PIN_AMP_1, HIGH);
+  digitalWrite(PIN_AMP_2, HIGH);
+  digitalWrite(PIN_AMP_3, HIGH);
+  digitalWrite(PIN_AMP_4, HIGH);
+  digitalWrite(PIN_AMP_5, HIGH);
   analogWrite(PIN_VOL_1, vol_1);
   analogWrite(PIN_VOL_2, vol_2);
   analogWrite(PIN_VOL_3, vol_3);
