f45bf067fe3f9b4cc83db248bdd9b7441bbd7bb1
Vladimir Vilisov <vvvalt@mail.ru>
2018-10-07 19:46:17 +0700

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,4 +1,122 @@
 //--------------------------------- Functions ---------------------------------//
+void callback_iobroker(String strTopic, String strPayload) {
+    if (strPayload == "true") {
+      Save();
+      mqtt.publish("myhome/Audio_Amplifier/save", "false");
+    }
+  if (strTopic == "myhome/Audio_Amplifier/shtd") {
+    if (strPayload == "true") {
+      digitalWrite(PIN_SHTD, HIGH);
+      shtd = 1;
+    } else {
+      digitalWrite(PIN_SHTD, LOW);
+      shtd = 0;
+    }
+    mqtt.publish("myhome/Audio_Amplifier/shtd", IntToBool(shtd));
+  }
+  if (strTopic == "myhome/Audio_Amplifier/fade") {
+    if (strPayload == "true") {
+      digitalWrite(PIN_FADE, HIGH);
+      fade = 1;
+    } else {
+      digitalWrite(PIN_FADE, LOW);
+      fade = 0;
+    }
+    mqtt.publish("myhome/Audio_Amplifier/fade", IntToBool(fade));
+  }
+  if (strTopic == "myhome/Audio_Amplifier/zone_1") {
+    if (strPayload == "false") {
+      digitalWrite(PIN_AMP_1, HIGH);
+      mqtt.publish("myhome/Audio_Amplifier/zone_1", "false");
+    } else {
+      digitalWrite(PIN_AMP_1, LOW);
+      mqtt.publish("myhome/Audio_Amplifier/zone_1", "true");
+    }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_2") {
+    if (strPayload == "false") {
+      digitalWrite(PIN_AMP_2, HIGH);
+      mqtt.publish("myhome/Audio_Amplifier/zone_2", "false");
+    } else {
+      digitalWrite(PIN_AMP_2, LOW);
+      mqtt.publish("myhome/Audio_Amplifier/zone_2", "true");
+    }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_3") {
+    if (strPayload == "false") {
+      digitalWrite(PIN_AMP_3, HIGH);
+      mqtt.publish("myhome/Audio_Amplifier/zone_3", "false");
+    } else {
+      digitalWrite(PIN_AMP_3, LOW);
+      mqtt.publish("myhome/Audio_Amplifier/zone_3", "true");
+    }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_4") {
+    if (strPayload == "false") {
+      digitalWrite(PIN_AMP_4, HIGH);
+      mqtt.publish("myhome/Audio_Amplifier/zone_4", "false");
+    } else {
+      digitalWrite(PIN_AMP_4, LOW);
+      mqtt.publish("myhome/Audio_Amplifier/zone_4", "true");
+    }
+  }
+  //////////////////////////
+  else if (strTopic == "myhome/Audio_Amplifier/zone_5") {
+    if (strPayload == "false") {
+      digitalWrite(PIN_AMP_5, HIGH);
+      mqtt.publish("myhome/Audio_Amplifier/zone_5", "false");
+    } else {
+      digitalWrite(PIN_AMP_5, LOW);
+      mqtt.publish("myhome/Audio_Amplifier/zone_5", "true");
+    }
+  }
+  /////////////////////////////////////////////////////////
+  int int_Payload;
+  if (strTopic == "myhome/Audio_Amplifier/vol_1") {
+    int_Payload = strPayload.toInt();
+    if (int_Payload >= 0 && int_Payload <= 255) {
+      analogWrite(PIN_VOL_1, int_Payload);
+      vol_1 = int_Payload;
+      mqtt.publish("myhome/Audio_Amplifier/vol_1", ToChar(strPayload));
+    }
+  }
+  else if (strTopic == "myhome/Audio_Amplifier/vol_2") {
+    int_Payload = strPayload.toInt();
+    if (int_Payload >= 0 && int_Payload <= 255) {
+      analogWrite(PIN_VOL_2, int_Payload);
+      vol_2 = int_Payload;
+      mqtt.publish("myhome/Audio_Amplifier/vol_2", ToChar(strPayload));
+    }
+  }
+  else if (strTopic == "myhome/Audio_Amplifier/vol_3") {
+    int_Payload = strPayload.toInt();
+    if (int_Payload >= 0 && int_Payload <= 255) {
+      analogWrite(PIN_VOL_3, int_Payload);
+      vol_3 = int_Payload;
+      mqtt.publish("myhome/Audio_Amplifier/vol_3", ToChar(strPayload));
+    }
+  }
+  else if (strTopic == "myhome/Audio_Amplifier/vol_4") {
+    int_Payload = strPayload.toInt();
+    if (int_Payload >= 0 && int_Payload <= 255) {
+      analogWrite(PIN_VOL_4, int_Payload);
+      vol_4 = int_Payload;
+      mqtt.publish("myhome/Audio_Amplifier/vol_4", ToChar(strPayload));
+    }
+  }
+  else if (strTopic == "myhome/Audio_Amplifier/vol_5") {
+    int_Payload = strPayload.toInt();
+    if (int_Payload >= 0 && int_Payload <= 255) {
+      analogWrite(PIN_VOL_5, int_Payload);
+      vol_5 = int_Payload;
+      mqtt.publish("myhome/Audio_Amplifier/vol_5", ToChar(strPayload));
+    }
+  }
+}
+
 char* ToChar (String intstr) {
   intstr.toCharArray(b, 4);
   return b;
@@ -8,7 +126,7 @@ char* IntToChar (int intV) {
   stringVar.toCharArray(b, 4);
   return b;
 }
-char* IntToBool (int r) {
+const char* IntToBool (int r) {
     if (r > 0){
       return "true";
     } else{
@@ -23,124 +141,4 @@ void Save(){
     EEPROM.write(5, byte(vol_5));
     EEPROM.write(6, byte(shtd));
     EEPROM.write(7, byte(fade));
-}
-
-void callback_iobroker(String strTopic, String strPayload) {
-  if (strTopic == "myhome/Audio_Amplifier/save") {
-    if (strPayload == "true") {
-      Save();
-      client.publish("myhome/Audio_Amplifier/save", "false");
-    }
-  }
-  if (strTopic == "myhome/Audio_Amplifier/shtd") {
-    if (strPayload == "true") {
-      digitalWrite(PIN_SHTD, true);
-      shtd = 1;
-    } else {
-      digitalWrite(PIN_SHTD, false);
-      shtd = 0;
-    }
-    client.publish("myhome/Audio_Amplifier/shtd", IntToBool(shtd));
-  }
-  if (strTopic == "myhome/Audio_Amplifier/fade") {
-    if (strPayload == "true") {
-      digitalWrite(PIN_FADE, true);
-      fade = 1;
-    } else {
-      digitalWrite(PIN_FADE, false);
-      fade = 0;
-    }
-    client.publish("myhome/Audio_Amplifier/fade", IntToBool(fade));
-  }
-  if (strTopic == "myhome/Audio_Amplifier/zone_1") {
-    if (strPayload == "false") {
-      digitalWrite(PIN_AMP_1, true);
-      client.publish("myhome/Audio_Amplifier/zone_1", "false");
-    } else {
-      digitalWrite(PIN_AMP_1, false);
-      client.publish("myhome/Audio_Amplifier/zone_1", "true");
-    }
-  }
-  //////////////////////////
-  else if (strTopic == "myhome/Audio_Amplifier/zone_2") {
-    if (strPayload == "false") {
-      digitalWrite(PIN_AMP_2, true);
-      client.publish("myhome/Audio_Amplifier/zone_2", "false");
-    } else {
-      digitalWrite(PIN_AMP_2, false);
-      client.publish("myhome/Audio_Amplifier/zone_2", "true");
-    }
-  }
-  //////////////////////////
-  else if (strTopic == "myhome/Audio_Amplifier/zone_3") {
-    if (strPayload == "false") {
-      digitalWrite(PIN_AMP_3, true);
-      client.publish("myhome/Audio_Amplifier/zone_3", "false");
-    } else {
-      digitalWrite(PIN_AMP_3, false);
-      client.publish("myhome/Audio_Amplifier/zone_3", "true");
-    }
-  }
-  //////////////////////////
-  else if (strTopic == "myhome/Audio_Amplifier/zone_4") {
-    if (strPayload == "false") {
-      digitalWrite(PIN_AMP_4, true);
-      client.publish("myhome/Audio_Amplifier/zone_4", "false");
-    } else {
-      digitalWrite(PIN_AMP_4, false);
-      client.publish("myhome/Audio_Amplifier/zone_4", "true");
-    }
-  }
-  //////////////////////////
-  else if (strTopic == "myhome/Audio_Amplifier/zone_5") {
-    if (strPayload == "false") {
-      digitalWrite(PIN_AMP_5, true);
-      client.publish("myhome/Audio_Amplifier/zone_5", "false");
-    } else {
-      digitalWrite(PIN_AMP_5, false);
-      client.publish("myhome/Audio_Amplifier/zone_5", "true");
-    }
-  }
-  /////////////////////////////////////////////////////////
-  int int_Payload;
-  if (strTopic == "myhome/Audio_Amplifier/vol_1") {
-    int_Payload = strPayload.toInt();
-    if (int_Payload >= 0 && int_Payload <= 255) {
-      analogWrite(PIN_VOL_1, int_Payload);
-      vol_1 = int_Payload;
-      client.publish("myhome/Audio_Amplifier/vol_1", ToChar(strPayload));
-    }
-  }
-  else if (strTopic == "myhome/Audio_Amplifier/vol_2") {
-    int_Payload = strPayload.toInt();
-    if (int_Payload >= 0 && int_Payload <= 255) {
-      analogWrite(PIN_VOL_2, int_Payload);
-      vol_2 = int_Payload;
-      client.publish("myhome/Audio_Amplifier/vol_2", ToChar(strPayload));
-    }
-  }
-  else if (strTopic == "myhome/Audio_Amplifier/vol_3") {
-    int_Payload = strPayload.toInt();
-    if (int_Payload >= 0 && int_Payload <= 255) {
-      analogWrite(PIN_VOL_3, int_Payload);
-      vol_3 = int_Payload;
-      client.publish("myhome/Audio_Amplifier/vol_3", ToChar(strPayload));
-    }
-  }
-  else if (strTopic == "myhome/Audio_Amplifier/vol_4") {
-    int_Payload = strPayload.toInt();
-    if (int_Payload >= 0 && int_Payload <= 255) {
-      analogWrite(PIN_VOL_4, int_Payload);
-      vol_4 = int_Payload;
-      client.publish("myhome/Audio_Amplifier/vol_4", ToChar(strPayload));
-    }
-  }
-  else if (strTopic == "myhome/Audio_Amplifier/vol_5") {
-    int_Payload = strPayload.toInt();
-    if (int_Payload >= 0 && int_Payload <= 255) {
-      analogWrite(PIN_VOL_5, int_Payload);
-      vol_5 = int_Payload;
-      client.publish("myhome/Audio_Amplifier/vol_5", ToChar(strPayload));
-    }
-  }
 }
